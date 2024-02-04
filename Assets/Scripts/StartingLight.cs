45b760784dbee508e18784bb2f679b221d98c512
AmirHossein M <amirgooli.maali@gmail.com>
2024-02-04 17:52:14 +0330

    add Stat sound

--- Assets/Scripts/StartingLight.cs
+++ Assets/Scripts/StartingLight.cs
@@ -9,6 +9,9 @@ public class StartingLight : MonoBehaviour
     public GameObject AmberLightOn;
     public GameObject GreenLightOff;
     public GameObject GreenLightOn;
+    public AudioSource Sound1;
+    public AudioSource Sound2;
+    public GameObject Go;
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +24,25 @@ public class StartingLight : MonoBehaviour
         yield return new WaitForSeconds(1);
         RightLightOff.SetActive(false);
         RightLightOn.SetActive(true);
+        Sound1.Play();
         yield return new WaitForSeconds(1);
         RightLightOff.SetActive(true);
         RightLightOn.SetActive(false);
+        Sound1.Play();
         AmberLightOff.SetActive(false);
         AmberLightOn.SetActive(true);
         yield return new WaitForSeconds(1);
         AmberLightOff.SetActive(true);
         AmberLightOn.SetActive(false);
+        Sound2.Play();
         GreenLightOff.SetActive(false);
         GreenLightOn.SetActive(true);
+        yield return new WaitForSeconds(.1f);
         Save.RaceStart = true;
-
+        Go.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Go.SetActive(false);
+        GreenLightOff.SetActive(true);
+        GreenLightOn.SetActive(false);
     }
 }
